4929198f87301bb8cfbb251b94b2e587ac16708b
diler546 <128365889+diler546@users.noreply.github.com>
2025-01-21 11:24:56 +0300

    Add files via upload

---
+++ Shuler_MasterPol/MasterShulerTest/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Shuler_MasterPol.Services;
+using Shuler_MasterPol.Models.Services;
+
+namespace MasterPolUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        /// <summary>
+        /// PKGH
+        /// Проверка метода CalculateMaterialAmount.
+        /// </summary>
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(MaterialManager.CalculateMaterialAmount(2, 3, 8, 1.4, 6.8), 393);
+        }
+    }
+}
